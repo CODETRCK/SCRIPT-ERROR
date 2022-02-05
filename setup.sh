9e48418d143c64e4d07871bad31def084528fc6a
adamtech <adamtech323@gmail.com>
2022-02-05 17:32:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,21 +29,9 @@ fi
 
 else
 clear
-
+mkdir /var/lib/premium-script;
 exit 0
 fi
-echo '============================================='
-echo '         Silakan Tunggu sebentar '
-echo 'Process Update & Upgrade Sedang Dijalankan '
-echo '============================================='
-sleep 2
-apt update && apt upgrade -y
-clear
-echo '============================================='
-echo '      Process Update & Upgrade Selesai '
-echo '============================================='
-sleep 2
-clear
 echo '============================================='
 echo '        Process Seterusnya Silakan Tunggu '
 echo '============================================='
